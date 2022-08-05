21e566c218bb861a1036970f8ef792fbf2335df6
Tilek2304 <email>
2022-08-05 19:47:17 +0600

    task2 added

---
+++ task2.py
@@ -0,0 +1,23 @@
+"""
+Дан список, содержащий в себе строки из нескольких слов. Нужно найти самое длинное слово из всех переданных строк
+
+Например:
+    in: [‘я любуюсь миром’, ‘лежу на боку’]
+    out: ‘любуюсь’
+"""
+max_num = 0
+dict_ = {}
+list_ = ["я любуюсь миром", "лежу на боку"]
+num_list = []
+for i in list_:
+    words = i.split()
+    for word in words:
+        if len(word)>max_num:
+            max_num = len(word)
+            dict_[word] = max_num
+print(max_num)
+print(dict_)
+
+        
+        
+
